7eb77501395375cf987e9817838b6766ec31b820
BlackKingDragon <sandersca1993@gmail.com>
2014-10-29 17:16:10 -0400

    more changes while trying to get the app on GitHub

--- gen/com/example/uscconnect/BuildConfig.java
+++ gen/com/example/uscconnect/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.test1;
+package com.example.uscconnect;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
