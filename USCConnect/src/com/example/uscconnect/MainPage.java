742d4c7feef557ec1c1e181c2b376622536e5897
Joseph <js.ammer@gmail.com>
2015-03-23 01:51:44 -0400

    added .csv downloading

--- USCConnect/src/com/example/uscconnect/MainPage.java
+++ USCConnect/src/com/example/uscconnect/MainPage.java
@@ -1,90 +1,184 @@
 package com.example.uscconnect;
 
-
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainPage extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	ProgressDialog mProgressDialog; // ### for .csv downloading
 
-		 
-        setContentView(R.layout.activity_main_page);
-        
-        Button Search = (Button)findViewById(R.id.button1);
-        Search.setOnClickListener(new View.OnClickListener() {
-        	@Override
-        	public void onClick(View view) {
-        		Intent myIntent = new Intent(view.getContext(), SearchPage.class);
-        		//ProgressDialog dialog = ProgressDialog.show(MainPage.this, "", 
-                  //      "Loading. Please wait...", true);
-        		startActivityForResult(myIntent, 0);
-        		
-        	}
-        });
-        
-        Button Log = (Button)findViewById(R.id.button2);
-        Log.setOnClickListener(new View.OnClickListener() {
-            @Override
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// ### for .csv downloading
+		mProgressDialog = new ProgressDialog(MainPage.this);
+		mProgressDialog.setMessage("Downloading database file...");
+		mProgressDialog.setIndeterminate(true);
+		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		// mProgressDialog.setCancelable(true);
+		final DownloadTask downloadTask = new DownloadTask(MainPage.this);
+		downloadTask
+				.execute("http://cse.sc.edu/~ammer/uscconnect_opportunities_1414761945.csv");
+		// ###
+
+		setContentView(R.layout.activity_main_page);
+
+		Button Search = (Button) findViewById(R.id.button1);
+		Search.setOnClickListener(new View.OnClickListener() {
+			@Override
 			public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), StudentLog.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
-        
-        Button About = (Button)findViewById(R.id.button3);
-        About.setOnClickListener(new View.OnClickListener() {
-            @Override
+				Intent myIntent = new Intent(view.getContext(),
+						SearchPage.class);
+				// ProgressDialog dialog = ProgressDialog.show(MainPage.this,
+				// "",
+				// "Loading. Please wait...", true);
+				startActivityForResult(myIntent, 0);
+
+			}
+		});
+
+		Button Log = (Button) findViewById(R.id.button2);
+		Log.setOnClickListener(new View.OnClickListener() {
+			@Override
 			public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), AboutPage.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
-        
-        Button Application = (Button)findViewById(R.id.button4);
-        Application.setOnClickListener(new View.OnClickListener() {
-            @Override
+				Intent myIntent = new Intent(view.getContext(),
+						StudentLog.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+
+		Button About = (Button) findViewById(R.id.button3);
+		About.setOnClickListener(new View.OnClickListener() {
+			@Override
 			public void onClick(View view) {
-        		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sc.edu/uscconnect/gldapplication")));
+				Intent myIntent = new Intent(view.getContext(), AboutPage.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
 
-            }
-        });
-      //  Parse.initialize(this, "uv0wiLDntmTYohfjvjJrICI6nSae1hFc20GPf9JJ", "yvbChURyeihU8wCB6dyLWwZjSwkU9V0l11oQqeA7");
-        
-        
-    }
+		Button Application = (Button) findViewById(R.id.button4);
+		Application.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri
+						.parse("https://www.sc.edu/uscconnect/gldapplication")));
 
+			}
+		});
+		// Parse.initialize(this, "uv0wiLDntmTYohfjvjJrICI6nSae1hFc20GPf9JJ",
+		// "yvbChURyeihU8wCB6dyLWwZjSwkU9V0l11oQqeA7");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_page, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_page, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	// ### for downloading .csv
+	// usually, subclasses of AsyncTask are declared inside the activity class.
+	// that way, you can easily modify the UI thread from here
+	private class DownloadTask extends AsyncTask<String, Integer, String> {
+
+		private Context context;
+		private PowerManager.WakeLock mWakeLock;
+
+		public DownloadTask(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		protected String doInBackground(String... sUrl) {
+			InputStream input = null;
+			OutputStream output = null;
+			HttpURLConnection connection = null;
+			try {
+				URL url = new URL(sUrl[0]);
+				connection = (HttpURLConnection) url.openConnection();
+				connection.connect();
+
+				// expect HTTP 200 OK, so we don't mistakenly save error report
+				// instead of the file
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+					return "Server returned HTTP "
+							+ connection.getResponseCode() + " "
+							+ connection.getResponseMessage();
+				}
+
+				// this will be useful to display download percentage
+				// might be -1: server did not report the length
+				int fileLength = connection.getContentLength();
+
+				// download the file
+				input = connection.getInputStream();
+				output = new FileOutputStream(
+						"/sdcard/uscconnect_opportunities_1414761945.csv");
+
+				byte data[] = new byte[4096];
+				long total = 0;
+				int count;
+				while ((count = input.read(data)) != -1) {
+					// allow canceling with back button
+					if (isCancelled()) {
+						input.close();
+						return null;
+					}
+					total += count;
+					// publishing the progress....
+					if (fileLength > 0) // only if total length is known
+						publishProgress((int) (total * 100 / fileLength));
+					output.write(data, 0, count);
+				}
+			} catch (Exception e) {
+				return e.toString();
+			} finally {
+				try {
+					if (output != null)
+						output.close();
+					if (input != null)
+						input.close();
+				} catch (IOException ignored) {
+				}
+
+				if (connection != null)
+					connection.disconnect();
+			}
+			return null;
+		}
+	}
 }
+
